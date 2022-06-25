fb0c54d5965d6fb7ec1d70c8f00297b26f774f55
Pedro Couto <pedrocoutosoares214@gmail.com>
2022-06-24 22:40:06 -0300

    feat: initial structure

---
+++ src/routes/route.tsx
@@ -0,0 +1,19 @@
+import {
+  BrowserRouter, 
+  Route, 
+  Routes
+} from "react-router-dom";
+
+import { Home } from "../pages/Home";
+import { Login } from "../pages/Login";
+
+export function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<Login />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
